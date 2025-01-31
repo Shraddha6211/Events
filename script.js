05c6361516c2084dedfb6e120925b26af8c8a564
Shraddha6211 <shraddhatiwari6211@gmail.com>
2025-01-31 22:58:11 +0545

    event update

---
+++ script.js
@@ -0,0 +1,10 @@
+let Btn1=document.querySelector("#Btn1");
+Btn1.onclick = () =>{
+    console.log("Btn was clicked");
+}
+Btn1.ondblclick=() =>{
+    let a =19;
+    a++;
+    console.log("Print the value of :")
+    console.log(a);
+}
